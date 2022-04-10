90d44fd84bce38f42a0d6df6c5e20c1005bf336f
trevis2 <trevis2@gmail.com>
2022-04-10 22:45:23 +0200

    modify

---
+++ 01-intro/main.cpp
@@ -0,0 +1,49 @@
+
+#include "std_lib_facilities.h"
+
+// commentare/decommentare piu righe ctrl+k+c/ctrl+k+u
+// formattare testo alt+shift+f
+
+int main() // C++ programs start by executing the function main
+{
+    cout << "Hello, World!\n"; // output “He llo, World!”
+    keep_window_open();        // wait for a character to be entered
+    return 0;
+}
+
+// 1. What is the purpose of the “Hello, World!” program?
+// test the correct setup of the environment
+
+// 2. Name the four parts of a function.
+// return type, name(or identifier) of the function, optional list of parameters, body of function
+
+// 3. Name a function that must appear in every C++ program.
+// main 
+
+// 4. In the “Hello, World!” program, what is the purpose of the line return 0;?
+// end the program
+
+// 5. What is the purpose of the compiler?
+// compiler takes the source code and create the object code(machine code or executable)
+
+// 6. What is the purpose of the #include directive?
+// Instructs the computer to make available facilities from a file called std_lib_facilities.h
+
+// 7. What does a .h suffix at the end of a file name signify in C++?
+// A header file contains definitions of terms that we use in our program.
+
+// 8. What does the linker do for your program?
+// link together the resulting object code files to form an executable program. 
+
+// 9. What is the difference between a source file and an object file?
+// source file is human readable(c++ file), object file is machine readable(machine code). source file -> compiler -> object file
+
+// 10. What is an IDE and what does it do for you?
+// IDE id Integrated development environment, simplify the programming
+// IDEs usually include an editor with helpful features like color coding to help distinguish between
+// comments, keywords, and other parts of your program source code, plus other facilities to help you
+// debug your code, compile it, and run it. Debugging is the activity of finding errors in a program and
+// removing them;
+
+// 11. If you understand everything in the textbook, why is it necessary to practice?
+// because is essential in the learning process and reinforce your practical programming skills and give you experience with programming environment tools.
