ffdce3a8fd7c8d2ac6b5129b022f0a377fdf132d
trevis2 <trevis2@gmail.com>
2022-09-30 23:50:09 +0200

    prima implementazione todolist

--- 03-basic-count-text/main.cpp
+++ 03-basic-count-text/main.cpp
@@ -1,8 +1,39 @@
 
 #include <iostream>
+using namespace std;
+
+string *addItem(string item, string list[])
+{
+    string todolist[sizeof(list) + 1];
+    for (int i = 0; i < sizeof(list); i++)
+    {
+        todolist[i] = list[i];
+    }
+    todolist[sizeof(list) + 1] = item;
+    return todolist;
+}
 
 int main(int argc, char **argv)
 {
-    std::cout << "Hello World" << std::endl;
+    cout << "Questo Programma gestisce una todo list " << std::endl;
+    cout << "Puoi aggiungere un oggetto o rimuoverlo selezionando + o - " << std::endl;
+
+    string todolist[1] = {"vuoto"};
+    cout << "La lista è:" << std::endl;
+    for (int i = 0; i < sizeof(todolist); i++)
+    {
+        cout << i << " - " << todolist[i] << std::endl;
+    }
+
+    while (true)
+    {
+        std::cout << "Seleziona + o - " << std::endl;
+        string i;
+        cin >> i;
+        if (i == "+" || i == "-")
+        {
+            break;
+        }
+    };
     return 0;
 }
