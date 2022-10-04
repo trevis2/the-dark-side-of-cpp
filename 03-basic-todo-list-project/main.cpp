f2bf57b80d06d6085b5bde2199a16d33d8287cb1
trevis2 <trevis2@gmail.com>
2022-10-04 14:45:26 +0200

    fix cicles and functions

--- 03-basic-todo-list-project/main.cpp
+++ 03-basic-todo-list-project/main.cpp
@@ -1,36 +1,57 @@
 
 #include <iostream>
+#include <iterator>
 #include <vector>
 
 void StampaListaInConsole(std::vector<std::string> lista)
 {
-    std::cout << "La Lista contiene " << lista.size() << " elementi" << std::endl;
-    std::cout << "Lista:" << std::endl;
-    for (int i = 0; i < lista.size(); i++)
+    std::cout << std::endl;
+    if (lista.size() == 0)
     {
-        std::cout << i << " - ";
-        std::cout << lista[i] << std::endl;
+        std::cout << "--- La Lista è vuota! ---" << std::endl;
     }
+    else if (lista.size() == 1)
+    {
+        std::cout << "--- La Lista contiene 1 elemento ---" << std::endl;
+        std::cout << 0 << " - ";
+        std::cout << lista[0] << std::endl;
+    }
+    else
+    {
+
+        std::cout << "--- La Lista contiene " << lista.size() << " elementi ---" << std::endl;
+        std::cout << "Lista:" << std::endl;
+        for (int i = 0; i < lista.size(); i++)
+        {
+            std::cout << i << " - ";
+            std::cout << lista[i] << std::endl;
+        }
+    }
+    std::cout << std::endl;
 };
 
 std::vector<std::string> InserisciElemento(std::vector<std::string> lista)
 {
-    std::cout << "digita l'elemento da inserire:" << std::endl;
+    std::cout << "*** MODE INSERIMENTO ***" << std::endl;
+    std::cout << "digita il testo del task da inserire:" << std::endl;
     std::string item;
-    std::cin >> item;
+    std::getline(std::cin, item);
     lista.push_back(item);
     return lista;
 };
 
-std::vector<std::string> RimuoviElemento(std::vector<std::string> lista, int i)
+std::vector<std::string> RimuoviElemento(std::vector<std::string> lista)
 {
-    vector::iterator q = lista.begin();
-    for (; q < i; q++)
-    {
-        // yes
-    }
+    using namespace std;
+    cout << "*** MODE RIMOZIONE ***" << endl;
+    cout << "seleziona la posizione dell'elemento che vuoi rimuovere:" << endl;
+    int k;
+    cin >> k;
 
-    vector::iterator p = vtr.erase(q);
+    vector<string>::iterator q = lista.begin();
+    advance(q, k);
+    vector<string>::iterator p = lista.erase(q);
+    return lista;
 };
 
 int main(int argc, char **argv)
@@ -40,31 +61,21 @@ int main(int argc, char **argv)
     std::vector<std::string> todolist;
     while (true)
     {
-        if (todolist.size() == 0)
+        StampaListaInConsole(todolist);
+        std::cout << "Inserisci '+' per aggiungere o '-' per rimuovere un task, per uscire x" << std::endl;
+        std::string j;
+        std::getline(std::cin, j);
+        if (j == "+")
         {
-            std::cout << "La Lista è vuota! inserisci il primo elemento:" << std::endl;
-            std::string item;
-            std::cin >> item;
-            todolist.push_back(item);
+            todolist = InserisciElemento(todolist);
         }
-        else
+        else if (j == "-")
         {
-            StampaListaInConsole(todolist);
-            std::cout << "Inserisci '+' per aggiungere o '-' per rimuovere" << std::endl;
-            std::string j;
-            std::cin >> j;
-            std::cout << "hai inserito: " << j << std::endl;
-            if (j == "+")
-            {
-                todolist = InserisciElemento(todolist);
-            }
-            else if (j == "-")
-            {
-                std::cout << "seleziona la posizione dell'elemento che vuoi rimuovere:" << std::endl;
-                int k;
-                std::cin >> k;
-                todolist = RimuoviElemento(todolist, k);
-            }
+            todolist = RimuoviElemento(todolist);
+        }
+        else if (j == "x")
+        {
+            std::exit(0);
         }
     }
 }
